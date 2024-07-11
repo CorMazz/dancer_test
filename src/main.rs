5360a648abf69959db64e6994ddc45937aeee267
CorMazz <corrado@mazzarelli.biz>
2024-07-11 21:23:34 +0000

    working implementation of rs256 auth backend

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,78 @@
-use askama_axum::Template;
-use axum::{
-    http::StatusCode,
-    response::{Html, IntoResponse},
-    routing::get, 
-    Router,
-    extract::Form,
+/// https://codevoweb.com/rust-and-axum-jwt-access-and-refresh-tokens/
+
+mod config;
+mod handler;
+mod jwt_auth;
+mod model;
+mod response;
+mod route;
+mod token;
+
+use config::Config;
+use std::sync::Arc;
+
+use axum::http::{
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+    HeaderValue, Method,
 };
-use serde::Deserialize;
+use dotenv::dotenv;
+use redis::Client;
+use route::create_router;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use tower_http::cors::CorsLayer;
 
-/// https://vitor.ws/blog/how_use_axum_askama_htmx
-
-#[derive(Template)]
-#[template(path = "root.html")]
-struct RootTemplate {}
-
-async fn root() -> impl IntoResponse {
-    let root: RootTemplate = RootTemplate { };
-    (StatusCode::OK, Html(root.render().unwrap()))
+pub struct AppState {
+    db: Pool<Postgres>,
+    env: Config,
+    redis_client: Client,
 }
-
-
-// #########################################################################################################################################
-// Leader Test Template
-// #########################################################################################################################################
-
-#[derive(Template)]
-#[template(path = "leader_test.html")]
-struct LeaderTestTemplate {
-    points: Vec<usize>,
-    dance_moves: Vec<&'static str>,
-}
-
-async fn leader_test_get() -> impl IntoResponse {
-    let points: Vec<usize> = vec![3, 2, 1];
-
-    let leader_test: LeaderTestTemplate = LeaderTestTemplate {
-        points: points,
-        dance_moves: vec!["Left Side Pass", "Right Side Pass", "Whip", "Reverse Whip"]
-    };
-
-    (StatusCode::OK, Html(leader_test.render().unwrap()))
-}
-
-// Later this can be refactored with a procedural macro to accept a global dance moves list that automagically
-// generates this struct, thus reducing repetition of the dance move points list
-#[derive(Debug, Deserialize)]
-struct DanceMovePoints {
-    left_side_pass: usize,
-    right_side_pass: usize,
-    whip: usize,
-    reverse_whip: usize,
-}
-
-async fn leader_test_post(Form(data): Form<DanceMovePoints>) {
-    println!("{:?}", data);
-}
-
-
-// #########################################################################################################################################
-// Main
-// #########################################################################################################################################
-
 
 #[tokio::main]
-async fn main() -> Result<(), ()> {
-    tracing_subscriber::fmt().init();
-    let port: &str = "5000";
+async fn main() {
+    dotenv().ok();
 
-    tracing::info!("router initialized, now listening on port {}", port);
+    let config = Config::init();
 
-    let listener: tokio::net::TcpListener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
+    let pool = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&config.database_url)
         .await
-        .unwrap();
+    {
+        Ok(pool) => {
+            println!("✅Connection to the database is successful!");
+            pool
+        }
+        Err(err) => {
+            println!("🔥 Failed to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
 
-    let app: Router = Router::new()
-        .route("/", get(root))
-        .route("/leader_test", get(leader_test_get).post(leader_test_post));
+    let redis_client = match Client::open(config.redis_url.to_owned()) {
+        Ok(client) => {
+            println!("✅Connection to the redis is successful!");
+            client
+        }
+        Err(e) => {
+            println!("🔥 Error connecting to Redis: {}", e);
+            std::process::exit(1);
+        }
+    };
 
-    axum::serve(listener, app).await.unwrap();
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
+        .allow_credentials(true)
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
 
-    Ok(())
-}
+    let app = create_router(Arc::new(AppState {
+        db: pool.clone(),
+        env: config.clone(),
+        redis_client: redis_client.clone(),
+    }))
+    .layer(cors);
 
-
-// Any filter defined in the module `filters` is accessible in your template.
-mod filters {
-    // This filter requires a `usize` input when called in templates
-    pub fn replace<T: std::fmt::Display>(s: T, original: &str, replace: &str) -> ::askama::Result<String> {
-        let s: String = s.to_string();
-        Ok(s.replace(original, replace))
-    }
+    println!("🚀 Server started successfully");
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    axum::serve(listener, app).await.unwrap()
 }
