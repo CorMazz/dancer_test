63205afb1d7ed6ec4fca516b385c2a4b8329989e
CorMazz <95447242+CorMazz@users.noreply.github.com>
2024-09-26 03:55:57 +0200

    migrated to UUID for all IDs (#27)

--- migrations/20240806094122_init.up.sql
+++ migrations/20240806094122_init.up.sql
@@ -10,7 +10,7 @@ SET pg_trgm.similarity_threshold = 0.3;
 
 CREATE TABLE
     "users" (
-        id UUID NOT NULL PRIMARY KEY DEFAULT (uuid_generate_v4()),
+        id UUID PRIMARY KEY DEFAULT (uuid_generate_v4()),
         first_name VARCHAR(100) NOT NULL,
         last_name VARCHAR(100) NOT NULL,
         email VARCHAR(255) NOT NULL UNIQUE,
@@ -26,7 +26,7 @@ CREATE TABLE
 CREATE INDEX users_email_idx ON users (email);
 
 CREATE TABLE testees (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT (uuid_generate_v4()),
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE
@@ -39,47 +39,47 @@ CREATE INDEX testees_email_trgm_idx ON testees USING gin (email gin_trgm_ops);
 
 
 CREATE TABLE tests (
-    id SERIAL PRIMARY KEY
+    id UUID PRIMARY KEY DEFAULT (uuid_generate_v4())
 );
 
 CREATE TABLE test_metadata (
-    id SERIAL PRIMARY KEY,       
-    test_id INTEGER NOT NULL REFERENCES tests(id), 
+    id UUID PRIMARY KEY DEFAULT (uuid_generate_v4()),       
+    test_id UUID NOT NULL REFERENCES tests(id), 
     test_name VARCHAR NOT NULL,    
     minimum_percent REAL NOT NULL,   
     max_score INTEGER NOT NULL,
     achieved_score INTEGER NOT NULL,             
-    testee_id INTEGER NOT NULL REFERENCES testees(id),              
+    testee_id UUID NOT NULL REFERENCES testees(id),              
     test_date TIMESTAMP NOT NULL,
     is_passing BOOLEAN NOT NULL,
-    proctor UUID NOT NULL REFERENCES users(id),
+    proctor_id UUID NOT NULL REFERENCES users(id),
     failure_explanation TEXT[]                 
 );
 
 -- TestTable (stores multiple tables associated with a test)
 CREATE TABLE test_tables (
-    id SERIAL PRIMARY KEY,
-    test_id INTEGER NOT NULL REFERENCES tests(id)
+    id UUID PRIMARY KEY DEFAULT (uuid_generate_v4()),
+    test_id UUID NOT NULL REFERENCES tests(id)
 );
 
 -- TestSection (stores multiple sections under a table)
 CREATE TABLE test_sections (
-    id SERIAL PRIMARY KEY,
-    table_id INTEGER REFERENCES test_tables(id), 
+    id UUID PRIMARY KEY DEFAULT (uuid_generate_v4()),
+    table_id UUID REFERENCES test_tables(id), 
     name VARCHAR NOT NULL
 );
 
 -- ScoringCategory (stores scoring categories for sections)
 CREATE TABLE scoring_categories (
-    id SERIAL PRIMARY KEY,
-    section_id INTEGER REFERENCES test_sections(id), 
+    id UUID PRIMARY KEY DEFAULT (uuid_generate_v4()),
+    section_id UUID REFERENCES test_sections(id), 
     name VARCHAR NOT NULL,
     values TEXT[] NOT NULL                            
 );
 
 CREATE TABLE competencies (
-    id SERIAL PRIMARY KEY,
-    section_id INTEGER NOT NULL REFERENCES test_sections(id),  
+    id UUID PRIMARY KEY DEFAULT (uuid_generate_v4()),
+    section_id UUID NOT NULL REFERENCES test_sections(id),  
     name VARCHAR NOT NULL,
     scores JSONB NOT NULL,                      -- Array of scores (2D array for Vec<Vec<i32>>)
     subtext TEXT,
@@ -90,15 +90,15 @@ CREATE TABLE competencies (
 );
 
 CREATE TABLE bonus_items (
-    id SERIAL PRIMARY KEY,
-    test_id INTEGER NOT NULL REFERENCES tests(id), 
+    id UUID PRIMARY KEY DEFAULT (uuid_generate_v4()),
+    test_id UUID NOT NULL REFERENCES tests(id), 
     name VARCHAR NOT NULL,
     score INTEGER NOT NULL,
     achieved BOOLEAN NOT NULL                    
 );
 
 CREATE TABLE queue (
-    testee_id INT NOT NULL REFERENCES testees(id),
+    testee_id UUID NOT NULL REFERENCES testees(id),
     test_definition_index INTEGER NOT NULL,
     added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (testee_id, test_definition_index)
